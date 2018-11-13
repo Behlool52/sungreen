8f6094255fa519665cbe4d20b24254092d94cf6f
behlool <behlool@centricsource.com>
2018-11-13 22:31:49 +0500

    3 files added

---
+++ selectronics.php
@@ -0,0 +1,159 @@
+<?php include 'includes/header.php'; ?>
+      
+      <!-- Main Content Starts here -->
+      
+     
+ <div class="crane-section">
+        <div class="crane-container">
+          <div class="vc_row wpb_row vc_row-fluid">
+            <div class="wpb_column vc_column_container vc_col-sm-12 vc_col-lg-offset-0 vc_col-lg-5 vc_col-md-offset-0 vc_col-md-5 vc_col-sm-offset-0 vc_col-xs-12">
+              <div class="vc_column-inner ">
+                <div class="wpb_wrapper">
+				
+				<div class="ult-spacer spacer-5bcb6a562656c" data-id="5bcb6a562656c" data-height="90" data-height-mobile="90" data-height-tab="90" data-height-tab-portrait="" data-height-mobile-landscape="" style="clear:both;display:block;"></div>
+
+  <section class="custom-padding" style="transform: none;">
+        <div class="container" style="transform: none;">
+            <!-- Row -->
+            <div class="row" style="transform: none;">
+			
+			
+			
+                <div class="col-lg-3 col-sm-4 col-xs-12" id="side-bar" style="position: relative; overflow: visible; box-sizing: border-box; min-height: 1px;">
+                    <div class="theiaStickySidebar" style="padding-top: 0px; padding-bottom: 1px; position: static; top: 1091px;">
+                        <div class="side-bar-services">
+                            <ul class="side-bar-list">
+                                
+                                <li class="active"><a href="#sp_pro" data-toggle="tab" aria-expanded="true">SP PRO SERIES INVERTERS</a></li>
+                                <li class=""><a href="#mygrid" data-toggle="tab" aria-expanded="false">MYGRID ENERGY CENTRES</a></li>
+				<li class=""><a href="#kaco" data-toggle="tab" aria-expanded="false">KACO POWADOR INVERTERS</a></li>
+								
+                                
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+				
+				
+				
+                <div class="col-lg-9 col-sm-8 col-xs-12">
+                    <div class="tab-content"> 
+						
+                        <div class="row tab-pane fade active in" id="sp_pro">
+
+                            <div class="col-md-12">
+                                <div class="about-title">
+
+
+<div class="col-md-3"><img src="https://earthsave.com.au/images/SPPRO.jpg" style="width: 100%; filter: opacity(1);" data-image-blur-on-load-update-occured="true" data-pagespeed-url-hash="2931501080" onload="pagespeed.CriticalImages.checkImageForCriticality(this);"></div>
+<div class="col-md-9">
+<h2>SP PRO SERIES INVERTERS</h2>                                   									
+<p>The SP PRO is a smarter multi-mode inverter which controls and manages every aspect of your energy needs. The SP PRO is suitable for both Off-Grid and Solar Hybrid (grid-connect) installations for residential, commercial and industrial projects. Our inverters provide true transition from a hybrid to an off-grid system (and vice versa) with a simple on-site setting change. Selectronic's inverters are made locally and installed globally. You can find SP PROs in the tropics of Indonesia, the mountains of the Himalayas, the deserts of UAE, the outback of Australia and on suburban homes throughout the world. Selectronic has you covered with these world-renowned inverters that offer grid independence, outstanding reliability, ultimate flexibility,retro-fit capability and a 10 year warranty and after sales support that is second to none. By incorporating a renewable energy source with the SP PRO and batteries, you can be sure you will never be without power.</p>
+</div>
+
+ 
+                                   
+                                </div>
+
+                            </div>
+                        </div>						
+						
+						
+						<div class="row tab-pane fade" id="mygrid">
+
+                            <div class="col-md-12">
+                                <div class="about-title">
+
+<div class="col-md-3"><img src="https://earthsave.com.au/images/myGridB.jpg" style="width: 100%; filter: opacity(1);" data-image-blur-on-load-update-occured="true" data-pagespeed-url-hash="724995604" onload="pagespeed.CriticalImages.checkImageForCriticality(this);"></div>
+<div class="col-md-9">
+ <h2>MYGRID ENERGY CENTRES</h2>
+                                   <p>A Selectronic myGrid battery kit enables you to store your excess renewable energy and use it at the time of your choice.<br><br>
+Regardless of whether you have an existing solar power system or require a new system, the myGrid kits can be easily integrated to allow you to immediately take control of your energy usage. The myGrid kit includes everything you need except for your renewables. Each myGrid kit has been tested by Selectronic to ensure all components are compatible and can be installed to local standards saving you time and money.</p>
+</div>
+                                   
+
+
+
+
+                                    
+                                </div>
+
+                            </div>
+                        </div>
+						
+						
+						<div class="row tab-pane fade" id="kaco">
+
+                            <div class="col-md-12">
+                                <div class="about-title"> 
+
+<div class="col-md-3"><img src="https://earthsave.com.au/images/kaco.jpg" style="width: 100%; filter: opacity(1);" data-image-blur-on-load-update-occured="true" data-pagespeed-url-hash="1463348236" onload="pagespeed.CriticalImages.checkImageForCriticality(this);"></div>
+<div class="col-md-9">
+
+
+ <h2>KACO POWADOR INVERTERS</h2>
+									<p>Manufacturing in Germany for over 100 years, KACO have a rich history in Quality High reliability inverters. The KACO Powador range is produced using 100% renewable energy. KACO have a long, illustrious history with Selectronic and have both assisted each other in the development of today’s product range. These inverters are Solar Hybrid ready and able to connect to your SP PRO battery based system now or any time in the future. Whether a Solar Hybrid system or full Off Grid, a Selectronic Certified KACO inverter will integrate seamlessly. A convenient night switch is placed at the bottom of the inverter which allows the informative LCD display to be read at night. For maximum flexibility the KACO range features both Transformer and Transformerless designs with efficiency typically around 96%.
+</p><ul>
+<li>Install today, add batteries tomorrow</li>
+<li>European made</li>
+<li>High efficiency</li>
+<li>10 year warranty</li>
+<li>Selectronic Certified.</li>
+<li>Easy to read LCD display with night light</li>
+<li>Wireless communications option to SP PRO</li>
+<li>IP54 rating</li>
+</ul>
+
+</div>
+ 
+                                    
+                                  
+                                   
+                                </div>
+
+                            </div>
+                        </div>
+
+                                </div>
+
+
+
+                            </div>
+
+                        </div>
+                    </div>
+                </section>
+
+   <!-- =-=-=-=-=-=-= Our Services-end =-=-=-=-=-=-= -->
+	</div>
+	</div>
+	</div>
+</div>
+</div>
+</div>
+
+    
+      
+      <!-- Main Content Ends here -->
+      
+      <div class="crane-section">
+        <div class="crane-container">
+          <div class="vc_row wpb_row vc_row-fluid">
+            <div class="wpb_column vc_column_container vc_col-sm-12">
+              <div class="vc_column-inner ">
+                <div class="wpb_wrapper">
+                  <div class="ult-spacer spacer-5bcb706e89f7c" data-id="5bcb706e89f7c" data-height="100" data-height-mobile="100" data-height-tab="100" data-height-tab-portrait="" data-height-mobile-landscape="" style="clear:both;display:block;"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <span class="cp-load-after-post"></span> </div>
+  </div>
+</div>
+
+
+
+
+<?php include 'includes/footer.php' ?>
---
+++ the-solaxbox.php
@@ -0,0 +1,55 @@
+<?php include 'includes/header.php'; ?>
+      
+      <!-- Main Content Starts here -->
+      
+     
+ <div class="crane-section">
+       <div class="container custom-padding">
+<div class="container">
+          
+            <div class="row">
+
+<div class="col-md-12">
+
+               
+                                <div class="col-md-4"><img src="https://earthsave.com.au/images/solax-box.png" data-image-blur-on-load-update-occured="true" style="filter: opacity(1); width:100%" data-pagespeed-url-hash="412369221" onload="pagespeed.CriticalImages.checkImageForCriticality(this);"></div>
+                                <div class="col-md-8">
+                                		<h2>THE SOLAXBOX</h2>
+                                       <ul>
+                                          <li>Designed in Germany</li>
+                                          <li>Integrated energy management system</li>
+                                          <li>Colour touch-screen display</li>
+                                          <li>High performance lithium-ion batteries</li>
+                                        </ul>
+                                        <p>The X-Hybrid works by storing surplus energy in batteries for later use, making it possible to utilise solar power time-independently by storing unused capacity. It converts and directs solar power to where it is needed, when it is needed. The X-Hybrid is also supplied with a built-in EPS, allowing the end-user to use their stored energy in the event of a power outage.</p>
+
+                                  </div>
+                                   
+                               
+                </div>
+                
+              </div>
+              </div>
+
+	</div>
+</div>
+</div>
+
+    
+      
+      <!-- Main Content Ends here -->
+      
+    
+      
+	  
+	  
+	  <span class="cp-load-after-post"></span> 
+	  
+	  </div>
+  </div>
+</div>
+
+
+
+
+<?php include 'includes/footer.php' ?>
